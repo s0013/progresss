8ab0274a2f071c59edce31ce5e4799b1a3aa9930
SHRADDHA SONAWANE <shraddhasonawane524@gmail.com>
2024-04-18 18:27:17 +0530

    yess1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import HorizontalLinearStepper from "./HorizontalLinearStepper";
-
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
-    <HorizontalLinearStepper/>
+    <Container>
+      <Box sx={{ bgcolor: '#dfe7ee' }} style={{padding: "30px 30px 30px 30px"}}>
+        <HorizontalLinearStepper />
+      </Box>
+      <Box />
+    </Container>
   );
 }
 
