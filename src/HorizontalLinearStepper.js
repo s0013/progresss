d3bbbeb6b54532231383f4ec54d3d84baf625186
SHRADDHA SONAWANE <shraddhasonawane524@gmail.com>
2024-04-18 19:09:12 +0530

    yess3

--- src/HorizontalLinearStepper.js
+++ src/HorizontalLinearStepper.js
@@ -1,116 +1,71 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
+import React, { useState } from 'react';
+import { Button, Stepper, Step, StepLabel } from '@mui/material';
+import Personal from './component/Personal';
+import Academic from './component/Academic';
+import Address from './component/Address';
+import Submission from './component/Submission';
 
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+function getSteps() {
+  return ['Personal Details', 'Academic Details', 'Address Details', 'Submission'];
+}
 
-export default function HorizontalLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
+const HorizontalLinearStepper = () => {
 
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
 
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = getSteps();
+
+  const getStepContent = (stepIndex) => {
+    switch (stepIndex) {
+      case 0:
+        return <Personal />;
+      case 1:
+        return <Academic />;
+      case 2:
+        return <Address />;
+      case 3:
+        return <Submission />;
+      default:
+        return 'Successfully Submitted!!';
+    }
   };
 
   const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
   };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
   return (
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
+    <div>
+     
+      <div className="stepperContainer">
+        <Stepper alternativeLabel activeStep={activeStep}>
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
             </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
+          ))}
+        </Stepper>
+      </div>
+      <div>{getStepContent(activeStep)}</div>
+      <div>
+        {activeStep !== steps.length && (
+          <div>
+            <Button disabled={activeStep === 0} onClick={handleBack} className="button">
               Back
             </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                Skip
-              </Button>
-            )}
-
-            <Button onClick={handleNext}>
+            <Button variant="contained" color="primary" onClick={handleNext} className="button">
               {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
             </Button>
-          </Box>
-        </React.Fragment>
-      )}
-    </Box>
+          </div>
+        )}
+      </div>
+    </div>
   );
-}
+};
+
+export default HorizontalLinearStepper;
